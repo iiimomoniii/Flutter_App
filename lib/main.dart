0c0fc71921b1a5f6061158098d03be2b2dcd0f67
Natdanai Thaiyala <natdanai_thaiyala@live.com>
2020-09-25 15:19:41 +0700

    Update Code
    สร้าง ROW ขึ้นมา

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CM Layout",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("CM Layout"),
+        ),
+      //สร้าง ROW ขึ้นมา 1 Row
+      body: Row(
+        //ภายใต้ ROW จะมี ข้อมูล Logo , Codemobiles , CM Devs
+        children: <Widget>[FlutterLogo(), Text("Codemobiles"), Text("CM Devs")],
+      ),
+      ),
+
+    );
+  }
+}
