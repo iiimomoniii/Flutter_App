0fc1cd6de83a2e9c43f0fc134b4c92e356d150a8
Natdanai Thaiyala <natdanai_thaiyala@live.com>
2020-09-25 13:45:40 +0700

    Update Code
    สร้าง Layout ที่มี Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+//จัดระยะเว้นขอบ กำหนดทิศทาของ container
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CM Layout",
+      home: Scaffold(
+        //ส่วน Header ของ Page
+        //กำหนด เป็น appBar
+        appBar: AppBar(
+          //เเสดงข้อความที่ Bar
+          title: Text("CM Layout"),
+        ),
+        //ส่วน ฺBody ของ Page
+        body: Container(
+          //กำหนดสีพื่นหลังของ Container
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
