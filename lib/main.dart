7e558b06032942538ab6c03ba69f38153fa869a8
Natdanai Thaiyala <natdanai_thaiyala@live.com>
2020-09-28 13:57:22 +0700

    Update Code
    สร้าง Listview โดยที่มี Title เเละ Detail

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,62 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CM Layout",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("CM Layout"),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(30),
+            child: ListView(
+              children: <Widget>[
+                FlutterLogo(
+                  size: 100,
+                ),
+                Text(
+                  "Flutter Online",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                    "Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase. \n\n\n"),
+                Text(
+                  "Fast Development",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                    "Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of fully-customizable widgets to build native interfaces in minutes. \n\n\n"),
+                Text(
+                  "Expressive and Flexible UI",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                    "Quickly ship features with a focus on native end-user experiences. Layered architecture allows for full customization, which results in incredibly fast rendering and expressive and flexible designs. \n\n\n"),
+                Text(
+                  "Native Performance",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                    "Flutter’s widgets incorporate all critical platform differences such as scrolling, navigation, icons and fonts, and your Flutter code is compiled to native ARM machine code using Dart's native compilers. \n\n\n")
+              ],
+            ),
+          )),
+    );
+  }
+}
