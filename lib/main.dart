7adde4851ca2bee5b0aac17f582c2dd0b7995684
Natdanai Thaiyala <natdanai_thaiyala@live.com>
2020-09-29 10:14:12 +0700

    Update Code
    สร้าง Container   headerSection, titleSection,buttonSection, courseSection

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,44 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  var _title = "CM Workshop Layout";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(_title),
+        ),
+        body: ListView(
+          children: <Widget>[
+            headerSection,
+            titleSection,
+            buttonSection,
+            courseSection
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget headerSection = Container(
+  height: 250,
+  color: Colors.blue,
+);
+Widget titleSection = Container(
+  height: 250,
+  color: Colors.red,
+);
+Widget buttonSection = Container(
+  height: 250,
+  color: Colors.green,
+);
+Widget courseSection = Container(
+  height: 250,
+  color: Colors.yellow,
+);
