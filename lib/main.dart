47aff53367eca345234dffa10a163b56eb96ebe0
Natdanai Thaiyala <natdanai_thaiyala@live.com>
2020-09-29 11:28:00 +0700

    Update Code
    เพิ่มรายละเอียดส่วน headerSection นำรูปภาพมาใส่ เเละ titleSection ใส่รข้อมูลประกอบภาพ

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget headerSection = Container(
-  height: 250,
-  color: Colors.blue,
+//เเสดงรูปภาพ
+Widget headerSection = Image.network(
+    "https://freedesignfile.com/upload/2018/05/Cartoon-kids-with-back-to-school-background-vector-04.jpg");
+
+//เเสดงเนื้อหาของรูป
+Widget titleSection = Padding(
+  padding: EdgeInsets.all(50),
+  child: Row(
+    children: <Widget>[
+      //ใช้ Expanded ดันเพื่อให้เกิดพื้นที่ระหว่าง ข้อมูลฝั่งซ้าย กับ ข้อมูลฝั่งขวา
+      Expanded(
+        //ข้อมูลฝั่งซ้าย
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "School",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Bangkok",
+              style: TextStyle(color: Colors.grey[500]),
+            )
+          ],
+        ),
+      ),
+      //ข้อมูลฝั่งขวา
+      Icon(
+        Icons.thumb_up,
+        color: Colors.blue,
+      ),
+      Container(
+        margin: EdgeInsets.only(left: 8),
+        child: Text("99"),
+      ),
+    ],
+  ),
 );
-Widget titleSection = Container(
-  height: 250,
-  color: Colors.red,
-);
+
 Widget buttonSection = Container(
   height: 250,
   color: Colors.green,
